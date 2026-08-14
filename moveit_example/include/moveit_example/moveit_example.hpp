661123679c96d10ab0239db4b1e5c0c66a9bdea9
Laszlo Mate <Laszlo.Mate@kuka.com>
2026-08-14 13:07:27 +0000

    format

--- moveit_example/include/moveit_example/moveit_example.hpp
+++ moveit_example/include/moveit_example/moveit_example.hpp
@@ -30,38 +30,35 @@
 #include "moveit_visual_tools/moveit_visual_tools.h"
 #include "rclcpp/rclcpp.hpp"
 
-class MoveitExample : public rclcpp::Node
-{
+class MoveitExample : public rclcpp::Node {
 public:
-  explicit MoveitExample(
-    const std::string & node_name = "moveit_example",
-    const std::string & planning_group = "manipulator")
-  : rclcpp::Node(node_name), planning_group_(planning_group)
-  {
-  }
+  explicit MoveitExample(const std::string &node_name = "moveit_example",
+                         const std::string &planning_group = "manipulator")
+      : rclcpp::Node(node_name), planning_group_(planning_group) {}
 
-  void initialize()
-  {
-    move_group_interface_ = std::make_shared<moveit::planning_interface::MoveGroupInterface>(
-      shared_from_this(), planning_group_);
+  void initialize() {
+    move_group_interface_ =
+        std::make_shared<moveit::planning_interface::MoveGroupInterface>(
+            shared_from_this(), planning_group_);
 
-    moveit_visual_tools_ = std::make_shared<moveit_visual_tools::MoveItVisualTools>(
-      shared_from_this(), "world", rviz_visual_tools::RVIZ_MARKER_TOPIC,
-      move_group_interface_->getRobotModel());
+    moveit_visual_tools_ =
+        std::make_shared<moveit_visual_tools::MoveItVisualTools>(
+            shared_from_this(), "world", rviz_visual_tools::RVIZ_MARKER_TOPIC,
+            move_group_interface_->getRobotModel());
 
     moveit_visual_tools_->deleteAllMarkers();
     moveit_visual_tools_->loadRemoteControl();
     moveit_visual_tools_->trigger();
 
     planning_scene_diff_publisher_ =
-      this->create_publisher<moveit_msgs::msg::PlanningScene>("planning_scene", 10);
+        this->create_publisher<moveit_msgs::msg::PlanningScene>(
+            "planning_scene", 10);
 
     move_group_interface_->setMaxVelocityScalingFactor(0.1);
     move_group_interface_->setMaxAccelerationScalingFactor(0.1);
   }
 
-  moveit_msgs::msg::RobotTrajectory::SharedPtr drawCircle()
-  {
+  moveit_msgs::msg::RobotTrajectory::SharedPtr drawCircle() {
     std::vector<geometry_msgs::msg::Pose> waypoints;
     moveit_msgs::msg::RobotTrajectory trajectory;
     geometry_msgs::msg::Pose msg;
@@ -73,33 +70,30 @@ public:
     msg.orientation.w = sqrt(2) / 2;
     msg.position.x = 0.4;
     // Define waypoints in a circle
-    for (int i = 0; i < 63; i++)
-    {
+    for (int i = 0; i < 63; i++) {
       msg.position.y = -0.2 + sin(0.1 * i) * 0.15;
       msg.position.z = 0.4 + cos(0.1 * i) * 0.15;
       waypoints.push_back(msg);
     }
 
     RCLCPP_INFO(LOGGER, "Start planning");
-    double fraction = move_group_interface_->computeCartesianPath(waypoints, 0.005, trajectory);
+    double fraction = move_group_interface_->computeCartesianPath(
+        waypoints, 0.005, trajectory);
     RCLCPP_INFO(LOGGER, "Planning done!");
 
-    if (fraction < 1)
-    {
-      RCLCPP_ERROR(LOGGER, "Could not compute trajectory through all waypoints!");
+    if (fraction < 1) {
+      RCLCPP_ERROR(LOGGER,
+                   "Could not compute trajectory through all waypoints!");
       return nullptr;
-    }
-    else
-    {
+    } else {
       return std::make_shared<moveit_msgs::msg::RobotTrajectory>(trajectory);
     }
   }
 
   moveit_msgs::msg::RobotTrajectory::SharedPtr planToPoint(
-    const Eigen::Isometry3d & pose,
-    const std::string & planning_pipeline = "pilz_industrial_motion_planner",
-    const std::string & planner_id = "PTP")
-  {
+      const Eigen::Isometry3d &pose,
+      const std::string &planning_pipeline = "pilz_industrial_motion_planner",
+      const std::string &planner_id = "PTP") {
     // Create planning request using pilz industrial motion planner
     move_group_interface_->setPlanningPipelineId(planning_pipeline);
     move_group_interface_->setPlannerId(planner_id);
@@ -107,50 +101,44 @@ public:
 
     moveit::planning_interface::MoveGroupInterface::Plan plan;
     RCLCPP_INFO(LOGGER, "Sending planning request");
-    if (!move_group_interface_->plan(plan))
-    {
+    if (!move_group_interface_->plan(plan)) {
       RCLCPP_INFO(LOGGER, "Planning failed");
       return nullptr;
-    }
-    else
-    {
+    } else {
       RCLCPP_INFO(LOGGER, "Planning successful");
-      return std::make_shared<moveit_msgs::msg::RobotTrajectory>(plan.trajectory);
+      return std::make_shared<moveit_msgs::msg::RobotTrajectory>(
+          plan.trajectory);
     }
   }
 
-  moveit_msgs::msg::RobotTrajectory::SharedPtr planToPosition(
-    const std::vector<double> & joint_pos, const std::string & planning_pipeline = "ompl",
-    const std::string & planner_id = "RRTConnectkConfigDefault")
-  {
+  moveit_msgs::msg::RobotTrajectory::SharedPtr
+  planToPosition(const std::vector<double> &joint_pos,
+                 const std::string &planning_pipeline = "ompl",
+                 const std::string &planner_id = "RRTConnectkConfigDefault") {
     move_group_interface_->setPlanningPipelineId(planning_pipeline);
     move_group_interface_->setPlannerId(planner_id);
     move_group_interface_->setJointValueTarget(joint_pos);
 
     moveit::planning_interface::MoveGroupInterface::Plan plan;
     RCLCPP_INFO(LOGGER, "Sending planning request");
-    if (!move_group_interface_->plan(plan))
-    {
+    if (!move_group_interface_->plan(plan)) {
       RCLCPP_INFO(LOGGER, "Planning failed");
       return nullptr;
-    }
-    else
-    {
+    } else {
       RCLCPP_INFO(LOGGER, "Planning successful");
-      return std::make_shared<moveit_msgs::msg::RobotTrajectory>(plan.trajectory);
+      return std::make_shared<moveit_msgs::msg::RobotTrajectory>(
+          plan.trajectory);
     }
   }
 
-  bool executeTrajectory(const moveit_msgs::msg::RobotTrajectory & trajectory)
-  {
+  bool executeTrajectory(const moveit_msgs::msg::RobotTrajectory &trajectory) {
     return static_cast<bool>(move_group_interface_->execute(trajectory));
   }
 
   moveit_msgs::msg::RobotTrajectory::SharedPtr planToPointUntilSuccess(
-    const Eigen::Isometry3d & pose,
-    const std::string & planning_pipeline = "pilz_industrial_motion_planner",
-    const std::string & planner_id = "PTP")
-  {
+      const Eigen::Isometry3d &pose,
+      const std::string &planning_pipeline = "pilz_industrial_motion_planner",
+      const std::string &planner_id = "PTP") {
     // Create planning request using given motion planner
     move_group_interface_->setPlanningPipelineId(planning_pipeline);
     move_group_interface_->setPlannerId(planner_id);
@@ -160,19 +148,18 @@ public:
     RCLCPP_INFO(LOGGER, "Sending planning request");
     moveit::core::MoveItErrorCode err_code;
     auto start = std::chrono::high_resolution_clock::now();
-    do
-    {
+    do {
       RCLCPP_INFO(LOGGER, "Planning ...");
       err_code = move_group_interface_->plan(plan);
     } while (err_code != moveit::core::MoveItErrorCode::SUCCESS);
     auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    auto duration =
+        std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
     RCLCPP_INFO(LOGGER, "Planning successful after %li ms", duration.count());
     return std::make_shared<moveit_msgs::msg::RobotTrajectory>(plan.trajectory);
   }
 
-  void AddObject(const moveit_msgs::msg::CollisionObject & object)
-  {
+  void AddObject(const moveit_msgs::msg::CollisionObject &object) {
     moveit_msgs::msg::PlanningScene planning_scene;
     planning_scene.name = "scene";
     planning_scene.world.collision_objects.push_back(object);
@@ -180,10 +167,10 @@ public:
     planning_scene_diff_publisher_->publish(planning_scene);
   }
 
-  void addRobotPlatform()
-  {
+  void addRobotPlatform() {
     moveit_msgs::msg::CollisionObject collision_object;
-    collision_object.header.frame_id = move_group_interface_->getPlanningFrame();
+    collision_object.header.frame_id =
+        move_group_interface_->getPlanningFrame();
     collision_object.id = "robot_stand";
     shape_msgs::msg::SolidPrimitive primitive;
     primitive.type = primitive.BOX;
@@ -206,11 +193,11 @@ public:
     AddObject(collision_object);
   }
 
-  void addCollisionBox(
-    const geometry_msgs::msg::Vector3 & position, const geometry_msgs::msg::Vector3 & size)
-  {
+  void addCollisionBox(const geometry_msgs::msg::Vector3 &position,
+                       const geometry_msgs::msg::Vector3 &size) {
     moveit_msgs::msg::CollisionObject collision_object;
-    collision_object.header.frame_id = move_group_interface_->getPlanningFrame();
+    collision_object.header.frame_id =
+        move_group_interface_->getPlanningFrame();
     collision_object.id = "collision_box";
     shape_msgs::msg::SolidPrimitive primitive;
     primitive.type = primitive.BOX;
@@ -233,16 +220,13 @@ public:
     AddObject(collision_object);
   }
 
-  void addPalletObjects()
-  {
-    for (int k = 0; k < 3; k++)
-    {
-      for (int j = 0; j < 3; j++)
-      {
-        for (int i = 0; i < 3; i++)
-        {
+  void addPalletObjects() {
+    for (int k = 0; k < 3; k++) {
+      for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < 3; i++) {
           moveit_msgs::msg::CollisionObject pallet_object;
-          pallet_object.header.frame_id = move_group_interface_->getPlanningFrame();
+          pallet_object.header.frame_id =
+              move_group_interface_->getPlanningFrame();
 
           pallet_object.id = "pallet_" + std::to_string(9 * k + 3 * j + i);
           shape_msgs::msg::SolidPrimitive primitive;
@@ -270,8 +254,7 @@ public:
     }
   }
 
-  void AttachObject(const std::string & object_id)
-  {
+  void AttachObject(const std::string &object_id) {
     moveit_msgs::msg::PlanningScene planning_scene;
     planning_scene.name = "scene";
     moveit_msgs::msg::AttachedCollisionObject attached_object;
@@ -280,15 +263,17 @@ public:
     attached_object.object.id = object_id;
 
     // Carry out the REMOVE + ATTACH operation
-    RCLCPP_INFO(LOGGER, "Attaching the object to the hand and removing it from the world.");
-    planning_scene.robot_state.attached_collision_objects.push_back(attached_object);
+    RCLCPP_INFO(
+        LOGGER,
+        "Attaching the object to the hand and removing it from the world.");
+    planning_scene.robot_state.attached_collision_objects.push_back(
+        attached_object);
     planning_scene.robot_state.is_diff = true;
     planning_scene.is_diff = true;
     planning_scene_diff_publisher_->publish(planning_scene);
   }
 
-  void DetachAndRemoveObject(const std::string & object_id)
-  {
+  void DetachAndRemoveObject(const std::string &object_id) {
     moveit_msgs::msg::PlanningScene planning_scene;
     planning_scene.name = "scene";
     moveit_msgs::msg::AttachedCollisionObject attached_object;
@@ -299,19 +284,22 @@ public:
 
     // Carry out the DETACH operation
     RCLCPP_INFO(LOGGER, "Detaching the object from the hand");
-    planning_scene.robot_state.attached_collision_objects.push_back(attached_object);
+    planning_scene.robot_state.attached_collision_objects.push_back(
+        attached_object);
     planning_scene.robot_state.is_diff = true;
     planning_scene.world.collision_objects.push_back(attached_object.object);
     planning_scene.is_diff = true;
     planning_scene_diff_publisher_->publish(planning_scene);
   }
 
-  void setOrientationConstraint(const geometry_msgs::msg::Quaternion & orientation)
-  {
+  void
+  setOrientationConstraint(const geometry_msgs::msg::Quaternion &orientation) {
     moveit_msgs::msg::OrientationConstraint orientation_constraint;
     moveit_msgs::msg::Constraints constraints;
-    orientation_constraint.header.frame_id = move_group_interface_->getPlanningFrame();
-    orientation_constraint.link_name = move_group_interface_->getEndEffectorLink();
+    orientation_constraint.header.frame_id =
+        move_group_interface_->getPlanningFrame();
+    orientation_constraint.link_name =
+        move_group_interface_->getEndEffectorLink();
     orientation_constraint.orientation = orientation;
     orientation_constraint.absolute_x_axis_tolerance = 0.2;
     orientation_constraint.absolute_y_axis_tolerance = 0.2;
@@ -324,44 +312,44 @@ public:
 
   void clearConstraints() { move_group_interface_->clearPathConstraints(); }
 
-  void drawTrajectory(const moveit_msgs::msg::RobotTrajectory & trajectory)
-  {
+  void drawTrajectory(const moveit_msgs::msg::RobotTrajectory &trajectory) {
     moveit_visual_tools_->deleteAllMarkers();
     moveit_visual_tools_->publishTrajectoryLine(
-      trajectory, moveit_visual_tools_->getRobotModel()->getJointModelGroup(planning_group_));
+        trajectory, moveit_visual_tools_->getRobotModel()->getJointModelGroup(
+                        planning_group_));
   }
 
-  void drawTitle(const std::string & text)
-  {
-    auto const text_pose = []
-    {
+  void drawTitle(const std::string &text) {
+    auto const text_pose = [] {
       auto msg = Eigen::Isometry3d::Identity();
       msg.translation().z() = 1.0;
       return msg;
     }();
-    moveit_visual_tools_->publishText(
-      text_pose, text, rviz_visual_tools::RED, rviz_visual_tools::XXLARGE);
+    moveit_visual_tools_->publishText(text_pose, text, rviz_visual_tools::RED,
+                                      rviz_visual_tools::XXLARGE);
   }
 
-  void addBreakPoint()
-  {
+  void addBreakPoint() {
     moveit_visual_tools_->trigger();
-    moveit_visual_tools_->prompt("Press 'Next' in the RvizVisualToolsGui window to execute");
+    moveit_visual_tools_->prompt(
+        "Press 'Next' in the RvizVisualToolsGui window to execute");
   }
 
-  std::shared_ptr<moveit::planning_interface::MoveGroupInterface> moveGroupInterface()
-  {
+  std::shared_ptr<moveit::planning_interface::MoveGroupInterface>
+  moveGroupInterface() {
     return move_group_interface_;
   }
 
-  const std::string & planningGroup() const { return planning_group_; }
+  const std::string &planningGroup() const { return planning_group_; }
 
 protected:
-  std::shared_ptr<moveit::planning_interface::MoveGroupInterface> move_group_interface_;
-  rclcpp::Publisher<moveit_msgs::msg::PlanningScene>::SharedPtr planning_scene_diff_publisher_;
+  std::shared_ptr<moveit::planning_interface::MoveGroupInterface>
+      move_group_interface_;
+  rclcpp::Publisher<moveit_msgs::msg::PlanningScene>::SharedPtr
+      planning_scene_diff_publisher_;
   std::shared_ptr<moveit_visual_tools::MoveItVisualTools> moveit_visual_tools_;
   const rclcpp::Logger LOGGER = rclcpp::get_logger("moveit_basic_plan");
   const std::string planning_group_;
 };
 
-#endif  // MOVEIT_EXAMPLE__MOVEIT_EXAMPLE_HPP_
+#endif // MOVEIT_EXAMPLE__MOVEIT_EXAMPLE_HPP_
